0a5c0b71f69bab2c0d7ad52acdbb6f903f893c7f
michalkandybowicz <eejdzent@gmail.com>
2020-02-26 14:57:05 +0100

    add: - homepage with: base information - page with: all books list - page with: detail book information
    Signed-off-by: michalkandybowicz <eejdzent@gmail.com>

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('books/', views.BookListView.as_view(), name='books'),
+    path('books/', views.BookListView.as_view(), {'my_template_name': 'some_path'}, name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), {'my_template_name': 'another_path'}, name='book-detail'),
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,9 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from catalog.models import Book, Author, BookInstance
 from django.views import generic
+
 
 # Create your views here.
 
@@ -31,4 +33,24 @@ def index(request):
 
 
 class BookListView(generic.ListView):
+    template_name = "book_list.html"
     model = Book
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['books_list'] = Book.objects.all()
+        print(context)
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = "book_detail.html"
+
+    def book_detail_view(request, primary_key):
+        try:
+            book = Book.objects.get(pk=primary_key)
+        except Book.DoesNotExist:
+            raise Http404('Book does not exist')
+
+        return render(request, 'book_detail.html', context={'book': book})
