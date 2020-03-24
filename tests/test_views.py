a12e333c40441d55cbc49da3dea6be97126273da
michalkandybowicz <eejdzent@gmail.com>
2020-03-24 10:08:29 +0100

    test push 4
    Signed-off-by: michalkandybowicz <eejdzent@gmail.com>

--- tests/test_views.py
+++ tests/test_views.py
@@ -28,19 +28,3 @@ class AuthorListViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'genre_list.html')
 
-    def test_pagination_is_ten(self):
-        response = self.client.get(reverse('genre'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('is_paginated' in response.context)
-        self.assertTrue(response.context['is_paginated'] == True)
-        self.assertTrue(len(response.context['genre_list']) == 10)
-
-    def test_lists_all_authors(self):
-        # Get second page and confirm it has (exactly) remaining 3 items
-        response = self.client.get(reverse('genre') + '?page=2')
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('is_paginated' in response.context)
-        self.assertTrue(response.context['is_paginated'] == True)  # (if cond is True / if cond: )
-        self.assertTrue(len(response.context['genre_list']) == 3)
-
-# tests 5
